022e284ee3754bd247100afd271c683da7419114
motiullahsajit <sajitmotiullah@gmail.com>
2024-02-09 20:14:28 +0600

    up

--- controllers/station.js
+++ controllers/station.js
@@ -59,27 +59,22 @@ const listTrainsAtStation = async (req, res) => {
   try {
     const { station_id } = req.params;
 
-    // Find the station
     const station = await Station.findOne({ station_id });
 
-    // If station not found, return 404 error
     if (!station) {
       return res.status(404).json({
         message: `Station with id: ${station_id} was not found`,
       });
     }
 
-    // Find trains that have a stop at the given station
     const trains = await Train.find({ "stops.station_id": station_id });
 
-    // Map the trains to the response model
     const responseTrains = trains.map((train) => ({
       train_id: train.train_id,
       arrival_time: getArrivalTime(train, station_id),
       departure_time: getDepartureTime(train, station_id),
     }));
 
-    // Sort the trains based on departure time and arrival time
     responseTrains.sort(
       (a, b) =>
         compareTimes(a.departure_time, b.departure_time) ||
@@ -87,7 +82,6 @@ const listTrainsAtStation = async (req, res) => {
         a.train_id - b.train_id
     );
 
-    // Respond with the list of trains at the station
     res.status(200).json({ station_id: station_id, trains: responseTrains });
   } catch (err) {
     console.error(err);
@@ -95,7 +89,6 @@ const listTrainsAtStation = async (req, res) => {
   }
 };
 
-// Function to compare time strings
 const compareTimes = (timeA, timeB) => {
   if (!timeA && !timeB) return 0;
   if (!timeA) return -1;
@@ -103,13 +96,11 @@ const compareTimes = (timeA, timeB) => {
   return timeA.localeCompare(timeB);
 };
 
-// Function to get arrival time of a train at a station
 const getArrivalTime = (train, stationId) => {
   const stop = train.stops.find((stop) => stop.station_id == stationId);
   return stop ? stop.arrival_time : null;
 };
 
-// Function to get departure time of a train from a station
 const getDepartureTime = (train, stationId) => {
   const stop = train.stops.find((stop) => stop.station_id == stationId);
   return stop ? stop.departure_time : null;
