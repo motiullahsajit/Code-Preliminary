a8e99a5c435fc4f23bef0723d077f12f4a41d86a
motiullahsajit <sajitmotiullah@gmail.com>
2024-02-09 19:49:00 +0600

    updated

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -24,8 +24,6 @@ const purchaseTicket = async (req, res) => {
         message: `recharge amount: ${shortageAmount} to purchase the ticket`,
       });
     }
-
-    // Find route and trains for the journey
     const route = await findRoute(station_from, station_to, time_after);
     if (!route) {
       return res.status(403).json({
@@ -33,20 +31,21 @@ const purchaseTicket = async (req, res) => {
       });
     }
 
-    // Deduct ticket cost from user's balance
-    console.log("ticket cost from", ticketCost);
     user.balance -= ticketCost;
     await user.save();
 
-    // Construct ticket object
     const ticket = {
       ticket_id: generateTicketID(),
       balance: user.balance,
       wallet_id: wallet_id,
-      stations: route,
+      stations: route.map((stop) => ({
+        station_id: stop.station_id,
+        train_id: stop.train_id ? stop.train_id : null,
+        departure_time: stop.departure_time,
+        arrival_time: stop.arrival_time,
+      })),
     };
 
-    // Respond with ticket
     res.status(201).json(ticket);
   } catch (err) {
     console.error(err);
@@ -56,30 +55,27 @@ const purchaseTicket = async (req, res) => {
 
 const calculateTicketCost = async (station_from, station_to) => {
   try {
-    // Find the train with the specified stations
     const train = await Train.findOne({
       "stops.station_id": { $in: [station_from, station_to] },
     });
 
     if (!train) {
-      throw new Error(
-        `no ticket available for station: ${station_from} to station: ${station_to}`
-      );
+      return res.status(403).json({
+        message: `no ticket available for station: ${station_from} to station: ${station_to}`,
+      });
     }
 
-    // Find the stops corresponding to the start and end stations
     const startStop = train.stops.find(
       (stop) => stop.station_id === station_from
     );
     const endStop = train.stops.find((stop) => stop.station_id === station_to);
 
     if (!startStop || !endStop) {
-      throw new Error(
-        `no ticket available for station: ${station_from} to station: ${station_to}`
-      );
+      return res.status(403).json({
+        message: `no ticket available for station: ${station_from} to station: ${station_to}`,
+      });
     }
 
-    // Calculate the fare for the journey
     let totalFare = 0;
     const startIndex = train.stops.indexOf(startStop);
     const endIndex = train.stops.indexOf(endStop);
@@ -99,19 +95,17 @@ const calculateTicketCost = async (station_from, station_to) => {
 
 const findRoute = async (station_from, station_to, time_after) => {
   try {
-    // Find the train with the specified stations and departure time after time_after
     const train = await Train.findOne({
       "stops.station_id": { $all: [station_from, station_to] },
       "stops.departure_time": { $gt: time_after },
     });
 
     if (!train) {
-      throw new Error(
-        `no ticket available for station: ${station_from} to station: ${station_to}`
-      );
+      return res.status(403).json({
+        message: `no ticket available for station: ${station_from} to station: ${station_to}`,
+      });
     }
 
-    // Find the index of the start and end stations in the stops array
     const startIndex = train.stops.findIndex(
       (stop) => stop.station_id === station_from
     );
@@ -120,16 +114,21 @@ const findRoute = async (station_from, station_to, time_after) => {
     );
 
     if (startIndex === -1 || endIndex === -1) {
-      throw new Error(
-        `no ticket available for station: ${station_from} to station: ${station_to}`
-      );
+      return res.status(403).json({
+        message: `no ticket available for station: ${station_from} to station: ${station_to}`,
+      });
     }
 
-    // Construct the route based on the found stops
-    const route = train.stops.slice(
-      Math.min(startIndex, endIndex),
-      Math.max(startIndex, endIndex) + 1
-    );
+    const route = train.stops
+      .slice(Math.min(startIndex, endIndex), Math.max(startIndex, endIndex) + 1)
+      .map((stop, index) => {
+        return {
+          station_id: stop.station_id,
+          train_id: train.train_id,
+          departure_time: index === 0 ? stop.departure_time : null,
+          arrival_time: index === 0 ? null : stop.arrival_time,
+        };
+      });
 
     return route;
   } catch (error) {
@@ -137,7 +136,6 @@ const findRoute = async (station_from, station_to, time_after) => {
   }
 };
 
-// Function to generate a unique ticket ID (for demonstration purposes)
 const generateTicketID = () => {
   return Math.floor(Math.random() * 1000) + 1;
 };
